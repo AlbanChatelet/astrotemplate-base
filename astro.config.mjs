26036eab5c71c26ce792f748dcb319b25d2845ba
AlbanChatelet <chateletalban20@gmail.com>
2025-02-05 09:25:00 +0100

    Ajout de Preact à mon projet Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 export default defineConfig({
   site: "https://quiet-dango-c029b4.netlify.app/",
+  integrations: [preact()],
 });
